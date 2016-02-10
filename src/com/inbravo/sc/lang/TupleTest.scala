aec2e76837799cba50f04f8e686963f2cd2be016
Amit Dixit <amtdxt@yahoo.com>
2016-02-10 18:20:09 +0530

    New Tuple example added
    New Tuple example added

---
+++ src/com/inbravo/sc/lang/TupleTest.scala
@@ -0,0 +1,15 @@
+package com.inbravo.sc.lang
+
+object TupleTest {
+
+  def main(args: Array[String]) {
+
+    /* Create a new Tuple */
+    val t = (4, 3, 2, 1)
+
+    val sum = t._1 + t._2 + t._3 + t._4
+
+    println("Tuple: " + t.getClass)
+    println("Sum of elements: " + sum)
+  }
+}
