2ff5ae284002281b65b2aaed8d5679d7c5f50c6b
Amit Dixit <amtdxt@yahoo.com>
2016-03-15 18:18:39 +0530

    Anagram++
    Anagram++

--- src/main/scala/com/inbravo/string/Anagram.scala
+++ src/main/scala/com/inbravo/string/Anagram.scala
@@ -9,6 +9,8 @@ object Anagram {
     val firstString = "KATAK";
     val secondString = "TAKAK";
 
+    println("firstString.type : " + firstString.isInstanceOf[String] + ", secondString.type : " + secondString.isInstanceOf[Int]);
+
     /* Check if both strings are anagram */
     if (checkIfAnagram(firstString, secondString)) {
 
@@ -32,7 +34,7 @@ object Anagram {
     var firstStringChars = firstString.toLowerCase().toCharArray();
     var secondStringChars = secondString.toLowerCase().toCharArray();
 
-    /* Apply Dual-Pivot Quick sort by Vladimir Yaroslavskiy, Jon Bentley, and Joshua Bloch */
+    /* Apply quick sort */
     scala.util.Sorting.quickSort(firstStringChars)
     scala.util.Sorting.quickSort(secondStringChars);
 
