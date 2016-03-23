821eabbc7839308b9985ba0d317237ebc3e22e81
Amit Dixit <amtdxt@yahoo.com>
2016-03-23 15:16:36 +0530

    Stack++

--- src/main/scala/com/inbravo/ds/stack/Stack.scala
+++ src/main/scala/com/inbravo/ds/stack/Stack.scala
@@ -13,50 +13,11 @@ final class Stack {
 
   /* Current index stack */
   private var currentIndex = new AtomicInteger(-1)
-
-  /**
-   * Put value at index + 1
-   *
-   * @param value
-   */
+  
   def push(value: Long) = this.storage(this.currentIndex.incrementAndGet()) = value
-
-  /**
-   * Method or API
-   *
-   * @param value
-   */
-  def pop(): Long = {
-
-    /* Return value at current index */
-    var poppedValue = this.storage(this.currentIndex.get())
-
-    /* Reset the current index */
-    this.storage(this.currentIndex.getAndDecrement()) = 0
-
-    return poppedValue
-  }
-
-  /**
-   * Return value at current index
-   *
-   * @param value
-   */
-  private def peek(): Long = return this.storage(currentIndex.get())
-
-  /**
-   * Check if index = max size
-   *
-   * @param value
-   */
+  def pop(): Long = return this.storage(this.currentIndex.getAndDecrement())
+  def peek(): Long = return this.storage(currentIndex.get())
   def isFull(): Boolean = return (this.currentIndex.get() == (this.storage.length - 1))
-
-  /**
-   * Check if index = -1
-   *
-   * @param value
-   */
   def isEmpty(): Boolean = return (currentIndex.get() == -1)
-
   override def toString(): String = return (java.util.Arrays.toString(this.storage))
 }
