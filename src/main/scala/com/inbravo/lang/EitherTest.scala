2542cbd51a839787a1f6e576815b92274c256aa2
Amit Dixit <amtdxt@yahoo.com>
2016-04-22 18:30:22 +0530

    EitherTest++

---
+++ src/main/scala/com/inbravo/lang/EitherTest.scala
@@ -0,0 +1,24 @@
+package com.inbravo.lang
+/**
+ *
+ */
+object EitherTest extends App {
+
+  /**
+   * A simple method to demonstrate how to declare that a method returns an Either
+   */
+  def divide(x: Int, y: Int): Either[String, Int] = {
+
+    if (y == 0) { Left("Dude, can't divide by 0") }
+    else { Right(x / y) }
+  }
+
+  // a few different ways to use Either, Left, and Right
+  println(divide(1, 0))
+  println(divide(1, 1))
+
+  divide(1, 0) match {
+    case Left(s)  => println("Answer: " + s)
+    case Right(i) => println("Answer: " + i)
+  }
+}
---
+++ src/test/scala/com/inbravo/number/HexTest.scala
@@ -0,0 +1,10 @@
+package com.inbravo.number
+
+object HexTest extends App {
+
+  /* Hex number */
+  val magic = 0xcafebabe
+
+  /* Scala always prints integer values in base 10 */
+  println(magic);
+}
