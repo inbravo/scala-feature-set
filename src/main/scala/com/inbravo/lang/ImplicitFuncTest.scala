ddab5c3115526c39784dd79e35ca7ac6adeacdd2
Amit Dixit <amtdxt@yahoo.com>
2016-06-29 11:59:26 +0530

    ImplicitTest++

---
+++ src/main/scala/com/inbravo/lang/ImplicitFuncTest.scala
@@ -0,0 +1,34 @@
+package com.inbravo.lang
+
+/**
+ * Say hello
+ */
+class Greeter(val name: String) {
+
+  def sayHello = "Hello " + name;
+}
+
+/**
+ *
+ */
+object ImplicitFuncTest {
+
+  /* Create new implicit greet method */
+  implicit def greet(name: String) = new Greeter(name)
+
+  /* Create new name */
+  val name = "InBravo"
+
+  /* Create new Greeter */
+  val greeter = new Greeter(name)
+
+  /* Square will implicitly take value from previously defined statement */
+  def main(args: Array[String]): Unit = {
+
+    /* Say hello using simple method call */
+    println(greeter.sayHello)
+
+    /* Say hello using implicit method call */
+    println(name.sayHello)
+  }
+}
---
+++ src/main/scala/com/inbravo/lang/ImplicitParamTest.scala
@@ -0,0 +1,14 @@
+package com.inbravo.lang
+
+/**
+ *
+ */
+object ImplicitParamTest {
+
+  implicit val value = 2
+
+  /* Square will implicitly take value from previously defined statment */
+  def main(args: Array[String]): Unit = println(square)
+
+  def square(implicit value: Int) = value * value
+}
