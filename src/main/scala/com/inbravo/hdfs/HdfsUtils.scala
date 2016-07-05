d2d3b7830c024ab08c11d3271801b044eb65ca75
Amit Dixit <amtdxt@yahoo.com>
2016-07-05 13:28:09 +0530

    HdfsUtils++
    HdfsUtils++

--- src/main/scala/com/inbravo/hdfs/HdfsUtils.scala
+++ src/main/scala/com/inbravo/hdfs/HdfsUtils.scala
@@ -2,19 +2,32 @@ package com.inbravo.hdfs
 
 object HdfsUtils {
 
-  var hdfsServiceURL: String = "hdfs://localhost:9000"
+  /**
+   * Create a file
+   */
+  def create(hdfsFile: String, hdfsServiceURL: String) = {
+
+    println("Creating file: " + hdfsFile + ", at: " + hdfsServiceURL)
+
+    /* Get Hadoop configuration */
+    val hadoopConf = new org.apache.hadoop.conf.Configuration()
+    val hdfs = org.apache.hadoop.fs.FileSystem.get(new java.net.URI(hdfsServiceURL), hadoopConf)
+
+    /* Delete the file */
+    try { hdfs.create(new org.apache.hadoop.fs.Path(hdfsFile), true) } catch { case _: Throwable => { println("Error while creating Hadoop file") } }
+  }
 
   /**
    * Delete a file
    */
   def delete(hdfsFile: String, hdfsServiceURL: String) = {
 
-    println("Cleaning the file: " + hdfsFile + " @ hdfsServiceURL: " + hdfsServiceURL)
+    println("Deleting file: " + hdfsFile + ", from: " + hdfsServiceURL)
 
     /* Get Hadoop configuration */
     val hadoopConf = new org.apache.hadoop.conf.Configuration()
     val hdfs = org.apache.hadoop.fs.FileSystem.get(new java.net.URI(hdfsServiceURL), hadoopConf)
-    
+
     /* Delete the file */
     try { hdfs.delete(new org.apache.hadoop.fs.Path(hdfsFile), true) } catch { case _: Throwable => { println("Error while deleting Hadoop file") } }
   }
