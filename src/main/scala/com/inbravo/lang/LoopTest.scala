1912c1cacf46c54b6ff1c328a306375e048f13c9
Amit Dixit <amtdxt@yahoo.com>
2016-07-29 17:00:52 +0530

    LoopTest++

--- src/main/scala/com/inbravo/lang/LoopTest.scala
+++ src/main/scala/com/inbravo/lang/LoopTest.scala
@@ -8,18 +8,30 @@ object LoopTest extends App {
   /* while loop */
   while (i < max) {
 
-    doStuff(i)
+    printIt(i)
     i += 1
   }
 
   /* for loop */
   for (i <- 0 until max) {
 
-    doStuff(i)
+    printIt(i)
   }
 
   /* For each */
-  (0 until max).foreach(doStuff)
+  (0 until max).foreach(printIt)
 
-  def doStuff(value: Int) = println(value)
+  /* Print from 1 to 10 */
+  for (i <- 1 to 10) yield printIt(i)
+
+  /* Array of 5 values */
+  val a = Array(1, 2, 3, 4, 5)
+
+  /* printIt */
+  for (e <- a) yield printIt(e)
+
+  /* printIt only for values greater than 2 */
+  for (e <- a if e > 2) yield printIt(e)
+
+  def printIt(value: Int) = println(value)
 }
