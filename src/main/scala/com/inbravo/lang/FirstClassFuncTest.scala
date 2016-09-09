9ac2f2687ea88c894f82f816cae4116ea22f1d99
Amit Dixit <amtdxt@yahoo.com>
2016-09-09 12:07:13 +0530

    First-class function++
    First-class function++

--- src/main/scala/com/inbravo/lang/FirstClassFuncTest.scala
+++ src/main/scala/com/inbravo/lang/FirstClassFuncTest.scala
@@ -5,71 +5,108 @@ object FirstClassFuncTest {
   /* Anonymous First-class function as a variable */
   var increment = (x: Int) => x + 1
 
-  /* Anonymous First-class function with name */
+  /* First-class function as a definition */
   def incrementAgain = (x: Int) => x + 1
 
-  /* Anonymous First-class function as a variable */
+  /* First-class function as a variable */
   var same = (x: Int) => x
 
-  /* Anonymous First-class function as a variable */
+  /* First-class function as a variable */
   var square = (x: Int) => x * x
 
-  /* Anonymous First-class function as a variable */
+  /* First-class function as a variable */
   var cube = (x: Int) => x * x * x
 
   def main(args: Array[String]): Unit = {
 
     /* Call variable as function */
-    println(increment(10))
+    println("increment:	" + increment(10))
 
     /* Call a definition */
-    println(incrementAgain(10))
+    println("incrementAgain:	" + incrementAgain(10))
 
     /* Sum of numbers from 1 to 5 = 15 */
-    println(anotherSum(same, 1, 5))
+    println("thirdSum:	" + thirdSum(same, 1, 5))
+
+    /* Sum of numbers from 1 to 5 = 15 */
+    println("secondSum:	" + secondSum(same)(1, 5))
 
     /* Sum of square of numbers from 1 to 5 = 55 */
-    println(anotherSum(square, 1, 5))
+    println("thirdSum:	" + thirdSum(square, 1, 5))
 
     /* Sum of cube of every numbers from 1 to 5 = 225 */
-    println(anotherSum(cube, 1, 5))
+    println("thirdSum:	" + thirdSum(cube, 1, 5))
 
     /* Sum of cube of every numbers from 1 to 5 = 225 */
-    println(sum(cube)(1, 5))
+    println("sum:	" + sum(cube)(1, 5))
   }
 
-  def sum(f: Int => Int)(a: Int, b: Int): Int = {
+  /*
+   * Currying	:	sum(inputFunction)(innerFunctionParams)
+   */
+  def sum(inputFunction: Int => Int)(a: Int, b: Int): Int = {
 
     /* Inner function definition to loop */
+    /* Two input parameters and one output parameter */
     def loop(a: Int, acc: Int): Int = {
 
       /* If first number > second number */
       if (a > b) {
-        acc
-      } /* Else perform the recursive loop again */ else {
-        loop(a + 1, f(a) + acc)
+
+        return acc
+      } else {
+
+        /* Else perform the recursive loop again */
+        return loop(a + 1, inputFunction(a) + acc)
       }
     }
 
-    /* Now user loop method to perform sum */
-    loop(a, 0)
+    /* Return loop method */
+    return loop(a, 0)
+  }
+
+  /*
+   * Currying	:	secondSum(inputFunction)	:	outputFunction
+   */
+  def secondSum(inputFunction: Int => Int): (Int, Int) => Int = {
+
+    /* Inner function definition to loop */
+    /* Two input parameters and one output parameter */
+    def innerSum(a: Int, b: Int): Int = {
+
+      /* If first number > second number */
+      if (a > b) {
+
+        return 0
+      } else {
+
+        /* Else perform the recursive loop again */
+        return inputFunction(a) + innerSum(a + 1, b)
+      }
+    }
+
+    /* Return loop method */
+    return innerSum
   }
 
   /* Another definition of sum function */
-  def anotherSum(f: Int => Int, a: Int, b: Int): Int = {
+  def thirdSum(inputFunction: Int => Int, a: Int, b: Int): Int = {
 
     /* Inner function definition to loop */
     def loop(a: Int, acc: Int): Int = {
 
       /* If first number > second number */
       if (a > b) {
-        acc
-      } /* Else perform the recursive loop again */ else {
-        loop(a + 1, f(a) + acc)
+
+        return acc
+      } else {
+
+        /* Else perform the recursive loop again */
+        return loop(a + 1, inputFunction(a) + acc)
       }
     }
 
-    /* Now user loop method to perform sum */
-    loop(a, 0)
+    /* Return loop method */
+    return loop(a, 0)
   }
 }
