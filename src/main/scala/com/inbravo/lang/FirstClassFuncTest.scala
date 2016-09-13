ec7d44695d39a8f001007900d81ad1368870c4ee
Amit Dixit <amtdxt@yahoo.com>
2016-09-13 14:26:01 +0530

    FirstClassFuncTest++
    FirstClassFuncTest++

--- src/main/scala/com/inbravo/lang/FirstClassFuncTest.scala
+++ src/main/scala/com/inbravo/lang/FirstClassFuncTest.scala
@@ -22,29 +22,56 @@ object FirstClassFuncTest {
     /* Call variable as function */
     println("increment:	" + increment(10))
 
-    /* Call a definition */
+    /* Call a definition as function */
     println("incrementAgain:	" + incrementAgain(10))
 
-    /* Sum of numbers from 1 to 5 = 15 */
-    println("thirdSum:	" + thirdSum(same, 1, 5))
+    /* Sum of cube will just return a function */
+    println("sum:	" + sum(cube))
+
+    /* Sum of cube of every numbers from 1 to 5 = 225 */
+    println("sum:	" + sum(cube)(1, 5))
 
     /* Sum of numbers from 1 to 5 = 15 */
     println("secondSum:	" + secondSum(same)(1, 5))
+
+    /* Sum of numbers from 1 to 5 = 15 */
+    println("thirdSum:	" + thirdSum(same, 1, 5))
 
     /* Sum of square of numbers from 1 to 5 = 55 */
     println("thirdSum:	" + thirdSum(square, 1, 5))
 
     /* Sum of cube of every numbers from 1 to 5 = 225 */
     println("thirdSum:	" + thirdSum(cube, 1, 5))
-
-    /* Sum of cube of every numbers from 1 to 5 = 225 */
-    println("sum:	" + sum(cube)(1, 5))
   }
 
   /*
-   * Currying	:	sum(inputFunction)(innerFunctionParams)
+   * sum(inputFunction which takes an integer and gives an integer)	:	outputFunction which takes two integers and gives one integer
    */
-  def sum(inputFunction: Int => Int)(a: Int, b: Int): Int = {
+  def sum(inputFunction: Int => Int): (Int, Int) => Int = {
+
+    /* Inner function definition to loop */
+    /* Two input parameters and one output parameter */
+    def innerSum(a: Int, b: Int): Int = {
+
+      /* If first number > second number */
+      if (a > b) {
+
+        return 0
+      } else {
+
+        /* Else perform the recursive innerSum */
+        return inputFunction(a) + innerSum(a + 1, b)
+      }
+    }
+
+    /* Return innerSum method */
+    return innerSum
+  }
+
+  /*
+   * secondSum(inputFunction)(outputFunctionInput): outputFunctionOutput
+   */
+  def secondSum(inputFunction: Int => Int)(a: Int, b: Int): Int = {
 
     /* Inner function definition to loop */
     /* Two input parameters and one output parameter */
@@ -65,31 +92,7 @@ object FirstClassFuncTest {
     return loop(a, 0)
   }
 
-  /*
-   * Currying	:	secondSum(inputFunction)	:	outputFunction
-   */
-  def secondSum(inputFunction: Int => Int): (Int, Int) => Int = {
-
-    /* Inner function definition to loop */
-    /* Two input parameters and one output parameter */
-    def innerSum(a: Int, b: Int): Int = {
-
-      /* If first number > second number */
-      if (a > b) {
-
-        return 0
-      } else {
-
-        /* Else perform the recursive loop again */
-        return inputFunction(a) + innerSum(a + 1, b)
-      }
-    }
-
-    /* Return loop method */
-    return innerSum
-  }
-
-  /* Another definition of sum function */
+  /* Currying :	Another definition of sum function */
   def thirdSum(inputFunction: Int => Int, a: Int, b: Int): Int = {
 
     /* Inner function definition to loop */
