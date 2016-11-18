db0a6bbfcc629f8967b452a92ab968b70f358464
Amit Dixit <amtdxt@yahoo.com>
2016-11-18 15:26:59 +0530

    SparkRollUpProcess++

--- src/main/scala/com/inbravo/spark/SparkRollUpProcess.scala
+++ src/main/scala/com/inbravo/spark/SparkRollUpProcess.scala
@@ -5,19 +5,22 @@ import org.apache.spark.{ SparkContext, SparkConf }
 /**
  * This class is going to exhibit OLAP RollUp functionality
  *
- * It has a constructor which expects SparkRollUpVars variables
+ * It has a constructor which expects ClassVars variables
  *
  * amit.dixit
  */
 class SparkRollUpProcess(classVars: ClassVars) {
 
   /* Create new Spark SQL context */
-  val sqlContext = new SQLContext(new SparkContext(new SparkConf().setAppName("SparkRollUpTest")));
+  val sqlContext = new SQLContext(new SparkContext(new SparkConf().setAppName("SparkRollUpProcess")))
 
   /**
    * Following method is the core of this class
    */
-  def rollup(rollUpVars: RollUpVars, tableName: String, countColumnName: String): RollUpVars = {
+  def rollup(tableName: String, countColumnName: String): RollUpOutput = {
+
+    /* Output object */
+    val output = new RollUpOutput
 
     try {
 
@@ -25,13 +28,13 @@ class SparkRollUpProcess(classVars: ClassVars) {
       val dataFrame = sqlContext.sql("SELECT * FROM " + tableName)
 
       /* Use this as output DataFrame */
-      this.classVars.tempDataFrame = dataFrame.asInstanceOf
+      output.dataFrame = dataFrame.asInstanceOf
 
       /* Get column count from DataFrame */
-      val columnCount: Int = this.classVars.tempDataFrame.columns.length
+      val columnCount: Int = output.dataFrame.columns.length
 
       /* Get 'Count' column value */
-      val count: Int = columnCount + this.classVars.tempDataFrame.col(countColumnName).asInstanceOf[Int]
+      val count: Int = columnCount + output.dataFrame.col(countColumnName).asInstanceOf[Int]
 
       /* Get the aggregated sum */
       this.classVars.sumTempZero = this.classVars.sumTempZero + count
@@ -39,12 +42,53 @@ class SparkRollUpProcess(classVars: ClassVars) {
       /* Print the output frame */
       println("RollUp resulted : " + this.classVars.sumTempZero)
 
+      /* Generate output reports */
+      finalize(output, count)
+
     } finally {
 
       println("rollup is completed...")
     }
 
-    return rollUpVars
+    return output
+  }
+
+  /**
+   * Following method is to build the RollUp output
+   */
+  def finalize(output: RollUpOutput, count: Int): Array[RollUpOutput] = {
+
+    /* Get all columns */
+    val columns: Array[String] = output.dataFrame.columns
+
+    /* Create output array */
+    val outputArray = new Array[RollUpOutput](columns.length)
+
+    /* Get all column/values */
+    for (n <- columns) {
+
+      output.columnName = n
+      output.columnValue = output.dataFrame.col(output.columnName).asInstanceOf[String]
+      output.count = count
+    }
+
+    /* Generate table */
+    this.generateTable(outputArray)
+
+    return outputArray
+  }
+
+  /* Generate RollUp data in a table */
+  def generateTable(outputArray: Array[RollUpOutput]) {
+
+    /* Create new output table */
+    sqlContext.sql("CREATE TABLE IF NOT EXISTS ROLLUP_OUT(COUNT INT, COLUMN_NAME STRING, COLUMN_VALUE STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
+
+    for (n <- outputArray) {
+
+      /* Insert data in output table */
+      sqlContext.sql("INSERT INTO ROLLUP_OUT(COUNT, COLUMN_NAME, COLUMN_VALUE) VALUES(" + n.count + "," + n.columnName + "," + n.columnValue + ")")
+    }
   }
 
   /**
@@ -53,10 +97,10 @@ class SparkRollUpProcess(classVars: ClassVars) {
   def main(args: Array[String]): Unit = {
 
     /* Create RollUpVars */
-    var rollUpVars: RollUpVars = null;
+    var rollUpVars: RollUpOutput = null;
 
     /* Call RollUp */
-    rollUpVars = rollup(rollUpVars, "TABLE_ONE", "COUNT")
+    rollUpVars = rollup("ROLLUP_IN", "COUNT")
 
     /* Print the output frame */
     println(rollUpVars)
@@ -69,7 +113,7 @@ class SparkRollUpProcess(classVars: ClassVars) {
 trait ClassVars {
 
   /* Variable for aggregation */
-  var sumTempZero: Long = 0
+  var sumTempZero: Long
 
   /* Variable for final output table */
   var tempDataFrame: DataFrame
@@ -80,10 +124,19 @@ trait ClassVars {
 /**
  *  This is the trait to withhold all RollUp program variables
  */
-trait RollUpVars {
+class RollUpOutput {
 
   /* Variable for final output table */
-  var outputDataFrame: DataFrame
+  var dataFrame: DataFrame = null
 
-  def print() = { println(outputDataFrame.printSchema()) }
+  /* Variable for final output table */
+  var columnName: String = null
+
+  /* Variable for final output table */
+  var columnValue: String = null
+
+  /* Output of aggregation */
+  var count: Long = 0L
+
+  def print() = { println(dataFrame.printSchema()) }
 }
