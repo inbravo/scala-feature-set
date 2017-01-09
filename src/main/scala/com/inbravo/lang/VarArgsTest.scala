475e77e4ba7c04cf9a4a1e7c8713c0734a3dc422
Amit Dixit <amtdxt@yahoo.com>
2017-01-09 18:27:54 +0530

    VarArgsTest++

--- src/main/scala/com/inbravo/lang/VarArgsTest.scala
+++ src/main/scala/com/inbravo/lang/VarArgsTest.scala
@@ -17,6 +17,7 @@ object VarArgsTest extends App {
   def echoDouble(args: Double*) = for (arg <- args) println(arg)
 
   /* To pass in an array, need to  append the argument with a colon and an `_*` symbol */
+  /* Append ': _*' to tell the compiler that you want the parameter to be considered an argument sequence */
   echo(Array("arr", "of", "strings"): _*)
   echoInt(Array(1, 2, 3, 4, 5, 6, 7, 8, 9): _*)
   echoDouble(Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0): _*)
