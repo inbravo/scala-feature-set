f8e830096bc6610719bf1f6f70f64433ed180637
Amit Dixit <amtdxt@yahoo.com>
2017-01-10 16:59:46 +0530

    ArrayBufferTest++

--- src/main/scala/com/inbravo/lang/ArrayBufferTest.scala
+++ src/main/scala/com/inbravo/lang/ArrayBufferTest.scala
@@ -25,6 +25,32 @@ object ArrayBufferTest {
     /* Removes the last five elements */
     b.trimEnd(5)
 
+    /* Insert before index 2 */
+    b.insert(2, 6)
+
+    /* ArrayBuffer(1, 1, 7, 8, 9, 6, 2) */
+    b.insert(2, 7, 8, 9)
+
+    /* Remove a element at a index or at a starting index */
+    b.remove(2)
+    b.remove(2, 3)
+
     println(b)
+
+    /* 0 until 10 is actually a method call 0.until(10) */
+    for (i <- 0 until b.length) {
+      println(i + ": " + b(i))
+    }
+
+    /* 0 until 10 is actually a method call 0.until(10) */
+    for (i <- 0.until(b.length)) {
+      println(i + ": " + b(i))
+    }
+
+    /* Implicitly converts a 'ArrayBuffer' to a 'java.util.List' */
+    import scala.collection.JavaConversions.bufferAsJavaList
+
+    /* The 'ArrayBuffer' is wrapped into an object of a Java class that implements the 'java.util.List' interface */
+    val pb = new ProcessBuilder(ArrayBuffer("ls", "-al", "/home/cay"))
   }
 }
