25e6db6b0c5ce8640c336eff032756f3e93ffd14
Amit Dixit <amtdxt@yahoo.com>
2017-03-06 11:00:57 +0530

    HelloWorld++

---
+++ src/main/scala/com/inbravo/lang/HelloWorld.scala
@@ -0,0 +1,18 @@
+package com.inbravo.lang
+
+/**
+ * First test program
+ *
+ * amit.dixit
+ */
+object HelloWorld {
+
+  /**
+   *  Entry point of program
+   */
+  def main(args: Array[String]): Unit = {
+
+    /* Control flow : scala.Predef.println --> scala.Console.println --> java.io.PrintStream.println */
+    println("HelloWorld")
+  }
+}
