9b96864f761d6af8294c138ea2b85736fcfb21fa
Amit Dixit <amtdxt@yahoo.com>
2017-03-22 16:08:31 +0530

    ValuesTest++
    ValuesTest++

--- src/main/scala/com/inbravo/lang/ValuesTest.scala
+++ src/main/scala/com/inbravo/lang/ValuesTest.scala
@@ -7,17 +7,47 @@ package com.inbravo.lang
  */
 object ValuesTest extends App {
 
-  /* Different Scala types, initialized with their maximum value */
+  /* Different Scala types, initialized with their maximum value and with their types (NAME:TYPE e.g. val maxByteValue: Byte) */
   val maxByteValue: Byte = Byte.MaxValue
-  val maxCharValue: Char = Char.MinValue
+  val maxCharValue: Char = Char.MaxValue
   val maxShortValue: Short = Short.MaxValue
   val maxIntValue: Int = Int.MaxValue
   val maxLongValue: Long = Long.MaxValue
-  val maxLloatValue: Float = Float.MaxValue
+  val maxFloatValue: Float = Float.MaxValue
   val maxDoubleValue: Double = Double.MaxValue
 
-  /* Print values. For Char minimum value, which is '\uFFFF', which is not a printable character by definition, so a trick is used */
-  println("Byte Max Value: " + maxByteValue + ", Char Max Value: " + (maxCharValue + 0) + ", Short Max Value: " + maxShortValue)
-  println("Int Max Value: " + maxIntValue + ", Long Max Value: " + maxLongValue)
-  print("Float Max Value: " + maxLloatValue + ", Double Value: " + maxDoubleValue)
+  /* Print values */
+  println("Byte Max Value: " + maxByteValue)
+
+  /* Trick is used to convert a Char to yeild numeric value */
+  println("Char Max Value: " + (maxCharValue + 0))
+  println("Short Max Value: " + maxShortValue)
+  println("Int Max Value: " + maxIntValue)
+  println("Long Max Value: " + maxLongValue)
+  println("Float Max Value: " + maxFloatValue)
+
+  /* Method print does not ends with a new line */
+  print("Double Max Value: " + maxDoubleValue)
+
+  /* Different Scala types, initialized with their minimum value and without their types */
+  val minByteValueWithoutType = Byte.MinValue
+  val minCharValueWithoutType = Char.MinValue
+  val minShortValueWithoutType = Short.MinValue
+  val minIntValueWithoutType = Int.MinValue
+  val minLongValueWithoutType = Long.MinValue
+  val minFloatValueWithoutType = Float.MinValue
+  val minDoubleValueWithoutType = Double.MinValue
+
+  /* Print values */
+  println("Byte Min Value: " + minByteValueWithoutType)
+
+  /* Trick is used to convert a Char to yeild numeric value */
+  println("Char Min Value: " + (minCharValueWithoutType + 0))
+  println("Short Min Value: " + minShortValueWithoutType)
+  println("Int Min Value: " + minIntValueWithoutType)
+  println("Long Min Value: " + minLongValueWithoutType)
+  println("Float Min Value: " + minFloatValueWithoutType)
+
+  /* Method print does not ends with a new line */
+  print("Double Min Value: " + minDoubleValueWithoutType)
 }
