43ea3322d603ad3fb6d86a5cb204b7c61d0a26cc
Amit Dixit <amtdxt@yahoo.com>
2017-06-20 16:54:40 +0530

    StringTest++
    StringTest++

--- src/main/scala/com/inbravo/lang/StringsTest.scala
+++ src/main/scala/com/inbravo/lang/StringsTest.scala
@@ -15,10 +15,6 @@ object StringsTest {
     val stringOne = "stringOne"
     val stringTwo = "stringTwo"
 
-    /* Two lists */
-    val listOne = List("1")
-    val listTwo = List("2")
-
     val contactOperationOne = stringOne + stringTwo
     val contactOperationTwo = stringOne ++ stringTwo
 
@@ -29,14 +25,17 @@ object StringsTest {
       println("Both operations ('+' & '++') performs differently on strings")
     }
 
+    /* Two lists */
+    val listOne = List("1")
+    val listTwo = List("2")
+
     /* Below given line will give compilation error */
-    /* val mergectOperationOne = listOne + listTwo */   
+    /* val mergectOperationOne = listOne + listTwo */
     println(listOne ++ listTwo)
-    
-    val stringToBeSplit = "One,Two,Three,Four,Five" 
+
+    val stringToBeSplit = "One,Two,Three,Four,Five"
     var data = stringToBeSplit.split(",")
     println(data(0))
     println(data(1))
-    
   }
 }
