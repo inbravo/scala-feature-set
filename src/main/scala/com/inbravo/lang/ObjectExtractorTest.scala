b9c59bf4dc30620981718ee5506789d43b230c83
Amit Dixit <amtdxt@yahoo.com>
2017-06-27 16:45:40 +0530

    ObjectExtractorTest++
    ObjectExtractorTest++

--- src/main/scala/com/inbravo/lang/ObjectExtractorTest.scala
+++ src/main/scala/com/inbravo/lang/ObjectExtractorTest.scala
@@ -34,25 +34,6 @@ object IPAddress {
   def unapply(ip: String): Option[(String, String, String, String)] = {
 
     val tokens = ip split "\\."
-    if (tokens.length == 4 && isValid(tokens)) Some(tokens(0), tokens(1), tokens(2), tokens(3)) else None
-  }
-
-  private def isValid(tokens: Array[String]): Boolean = {
-
-    tokens forall {
-
-      /* Apply logic for all elements of array */
-      elem =>
-
-        try {
-          val intValue = elem.toInt
-
-          intValue >= 0 && intValue <= 255
-        } catch {
-
-          /* return false for any error */
-          case _ => false
-        }
-    }
+    if (tokens.length == 4) Some(tokens(0), tokens(1), tokens(2), tokens(3)) else None
   }
 }
